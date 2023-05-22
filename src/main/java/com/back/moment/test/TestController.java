a6560a2062809048b2b9a26160be385a98d568a5
seokscirbe <seokscribe1970@gmail.com>
2023-05-22 11:15:04 +0900

    Test: CI/CD 테스트 컨트롤러 추가

---
+++ src/main/java/com/back/moment/test/TestController.java
@@ -0,0 +1,12 @@
+package com.back.moment.test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+}
