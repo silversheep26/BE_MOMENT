53c623d29a9eeec8efb17cf6432443adb5d99886
seokscirbe <seokscribe1970@gmail.com>
2023-05-22 11:36:17 +0900

    Chore: deploy.sh 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,7 +2,7 @@
 REPOSITORY=/home/ubuntu/moment
 cd $REPOSITORY
 
-APP_NAME=settings
+APP_NAME=moment
 JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep 'SNAPSHOT.jar' | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
