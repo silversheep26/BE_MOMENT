c8a5d0cfcf2a6b71f7c1c775d0c4ad34914a0657
Kim Eunyang <awindya@naver.com>
2023-05-23 16:50:41 +0900

    feat(spring) : 회원가입, 로그인 구현

--- src/main/java/com/back/moment/users/entity/RoleEnum.java
+++ src/main/java/com/back/moment/users/entity/RoleEnum.java
@@ -1,6 +1,21 @@
 package com.back.moment.users.entity;
 
 public enum RoleEnum {
-    MODEL,
-    PHOTOGRAPHER
+    MODEL(Authority.MODEL),
+    PHOTOGRAPHER(Authority.PHOTOGRAPHER);
+
+    private final String authority;
+
+    RoleEnum(String authority) {
+        this.authority = authority;
+    }
+
+    public String getAuthority(){
+        return this.authority;
+    }
+
+    public static class Authority{
+        public static final String MODEL = "MODEL";
+        public static final String PHOTOGRAPHER = "PHOTOGRAPHER";
+    }
 }
--- src/main/java/com/back/moment/users/entity/Users.java
+++ src/main/java/com/back/moment/users/entity/Users.java
@@ -3,8 +3,8 @@ package com.back.moment.users.entity;
 import com.back.moment.boards.entity.Board;
 import com.back.moment.love.entity.Love;
 import com.back.moment.recommend.entity.Recommend;
+import com.back.moment.users.dto.SignupRequestDto;
 import jakarta.persistence.*;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -22,36 +22,36 @@ public class Users {
     private Long id;
 
     @Column(nullable = false)
-    private String email;
+    private String email;  // 이메일
 
     @Column(nullable = false)
-    private String nickName;
+    private String nickName;  // 닉네임
 
     @Column(nullable = false)
-    private String password;
+    private String password;  // 비밀번호
 
     @Column(nullable = false)
     @Enumerated(value = EnumType.STRING)
-    private SexEnum sex;
+    private SexEnum sex;  // 성별
 
     @Column
-    private String profileImg;
+    private String profileImg;  // 프로필 사진
 
     @Column(nullable = false)
     @Enumerated(value = EnumType.STRING)
-    private RoleEnum role;
+    private RoleEnum role;  // 모델 또는 작가
 
     @OneToMany(mappedBy = "users", cascade = CascadeType.ALL)
-    private List<Love> loveList = new ArrayList<>();
+    private List<Love> loveList = new ArrayList<>();  //좋아요
 
     @OneToMany(mappedBy = "users", cascade = CascadeType.ALL)
     private List<Board> boardList = new ArrayList<>();
 
     @OneToMany(mappedBy = "recommender", cascade = CascadeType.ALL)
-    private List<Recommend> recommenderList = new ArrayList<>();
+    private List<Recommend> recommenderList = new ArrayList<>();  // 추천 하는 사람 -> 추천 두번 누르면 취소 돼야 하지 않을까?
 
     @OneToMany(mappedBy = "recommended", cascade = CascadeType.ALL)
-    private List<Recommend> recommendedList = new ArrayList<>();
+    private List<Recommend> recommendedList = new ArrayList<>();  // 추천 받은 사람
 
     private Users(String email, String nickName, String password, SexEnum sex, String profileImg, RoleEnum role){
         this.email = email;
@@ -61,4 +61,13 @@ public class Users {
         this.profileImg = profileImg;
         this.role = role;
     }
+
+    public void saveUsers(SignupRequestDto requestDto, SexEnum sex, RoleEnum role) {
+        this.email = requestDto.getEmail();
+        this.password = requestDto.getPassword();
+        this.nickName = requestDto.getNickName();
+        this.sex = sex;
+        this.role = role;
+    }
+
 }
