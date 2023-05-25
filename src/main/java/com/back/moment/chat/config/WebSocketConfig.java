3d92dd18a1fb786a01dd69479a23a17c2f7ed19a
seokscirbe <seokscribe1970@gmail.com>
2023-05-25 10:41:46 +0900

    Chore: WebSocketConfig 설정파일 추가

---
+++ src/main/java/com/back/moment/chat/config/WebSocketConfig.java
@@ -0,0 +1,22 @@
+package com.back.moment.chat.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/sub"); //받기
+        registry.setApplicationDestinationPrefixes("/pub"); //보내기
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws-edit").setAllowedOriginPatterns("*").withSockJS();
+    }
+}
