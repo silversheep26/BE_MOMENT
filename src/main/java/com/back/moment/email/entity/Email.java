1e412a46d595778e7add059db2acaf005ec9e19d
seokscirbe <seokscribe1970@gmail.com>
2023-05-25 23:30:37 +0900

    Feat: Email 엔티티 추가

---
+++ src/main/java/com/back/moment/email/entity/Email.java
@@ -0,0 +1,35 @@
+package com.back.moment.email.entity;
+
+import com.back.moment.email.dto.EmailRequestDto;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Email {
+    @Id
+    @Column(nullable = false, unique = true)
+    private String email;
+    private String code;
+
+    private Email(String email) {
+        this.email = email;
+    }
+
+    private void setCode(String code) {
+        this.code = code;
+    }
+
+    public static Email saveEmail(EmailRequestDto requestDto) {
+        return new Email(requestDto.getEmail());
+    }
+
+    public static void updateCode(Email email, String code) {
+        email.setCode(code);
+    }
+
+}
