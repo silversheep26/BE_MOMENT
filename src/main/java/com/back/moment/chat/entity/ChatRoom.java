f0feb83ec45bc2badc2eb816d9828a3ecb2159ff
seokscirbe <seokscribe1970@gmail.com>
2023-05-27 11:56:53 +0900

    Refactor: ChatRoom 엔티티 수정
    Body: 전반적인 필드값을 변경하고 , User와의 연관관계를 추가했다. 또한 canUserOneSee , can UserTwoSee , lastMessageAt 필드를 추가

--- src/main/java/com/back/moment/chat/entity/ChatRoom.java
+++ src/main/java/com/back/moment/chat/entity/ChatRoom.java
@@ -4,21 +4,40 @@ import com.back.moment.users.entity.Users;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
 
 @Entity
 @NoArgsConstructor
 @Getter
-public class ChatRoom {
+public class ChatRoom{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private Long userA;
-    private Long userB;
-    private ChatRoom(Long userA, Long userB) {
-        this.userA = userA;
-        this.userB = userB;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "userOne_id")
+    private Users userOne;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "userTwo_id")
+    private Users userTwo;
+    private Boolean canUserOneSee = true; // 유저1이 이 채팅방을 지웠는지 여부 확인
+    private Boolean canUserTwoSee = true; // 유저2가 이 채팅방을 지웠는지 여부 확인
+    @Version
+    private LocalDateTime lastMessageAt; // 가장 최근에 채팅이 생성된 시간을 나타냄
+    private ChatRoom(Users userOne , Users userTwo) {
+        this.userOne = userOne;
+        this.userTwo = userTwo;
     }
-    public static ChatRoom of(Long userA,Long userB){
-        return new ChatRoom(userA,userB);
+    public static ChatRoom of(Users user1,Users user2){
+        return new ChatRoom(user1,user2);
+    }
+
+    public void updateCanUserOneSee(){
+        this.canUserOneSee = false;
+    }
+    public void updateCanUserTwoSee(){
+        this.canUserTwoSee = false;
+    }
+    public void updateLastMessageAt(LocalDateTime localDateTime){
+        this.lastMessageAt = localDateTime;
     }
 }
