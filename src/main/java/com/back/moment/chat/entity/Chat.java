27ba3502ffa1a53cdd82dd8934d87db67b763cd4
seokscirbe <seokscribe1970@gmail.com>
2023-05-27 11:51:23 +0900

    Fix: Chat 엔티티 수정
    Body: readStatus , userOneCanSee , userTwoCanSee 필드 추가

--- src/main/java/com/back/moment/chat/entity/Chat.java
+++ src/main/java/com/back/moment/chat/entity/Chat.java
@@ -11,20 +11,33 @@ import java.time.LocalDateTime;
 public class Chat {
     @Id
     private String id;
-    private String msg;
-    private Long senderId;
-    private Long roomId;
+    private String message;
+    private Long senderId; // 채팅을 보내는 유저의 아이디
+    private Long receiverId; // 채팅을 받는 유저의 아이디
+    private Long chatRoomId;
     private LocalDateTime createdAt;
+    private Boolean readStatus = false; // 채팅의 읽음 , 읽지않음 표시
+    private Boolean userOneCanSee = true; // 유저1이 볼 수 있는지 판별 , 예를들어 유저1이 채팅목록에서 삭제했는지 여부
+    private Boolean userTwoCanSee = true; // 유저2가 볼 수 있는지 판별 , 예를들어 유저1이 채팅목록에서 삭제했는지 여부
 
-    private Chat(String msg, Long senderId, Long roomId, LocalDateTime createdAt) {
-        this.msg = msg;
+    private Chat(String message, Long senderId, Long roomId, LocalDateTime createdAt) {
+        this.message = message;
         this.senderId = senderId;
-        this.roomId = roomId;
+        this.chatRoomId = roomId;
         this.createdAt = createdAt;
     }
 
-    public static Chat of(String msg, Long senderId, Long roomId, LocalDateTime createdAt){
-        return new Chat(msg,senderId,roomId,createdAt);
+    public static Chat of(String message, Long senderId, Long roomId, LocalDateTime createdAt){
+        return new Chat(message,senderId,roomId,createdAt);
+    }
+    public void updateReadStatus(){
+        this.readStatus = true;
+    } // 읽음 , 읽지않음 업데이트 표시
+    public void updateUserOneCanSee(){ // 삭제시 여부
+        this.userOneCanSee = false;
+    }
+    public void updateUserTwoCanSee(){
+        this.userTwoCanSee = false;
     }
 }
 
