838d3cdc7c5402180dce39fd1ac4c58ce56a1820
seokscirbe <seokscribe1970@gmail.com>
2023-05-27 11:57:49 +0900

    Refactor: ChatService 대부분 로직 변경
    Body: 전반적인 로직이 모두 변경되었다. 각 메서드에 주석으로 설명을 달아놓았다.

--- src/main/java/com/back/moment/chat/service/ChatService.java
+++ src/main/java/com/back/moment/chat/service/ChatService.java
@@ -3,16 +3,25 @@ package com.back.moment.chat.service;
 import com.back.moment.chat.dto.ChatRequestDto;
 import com.back.moment.chat.dto.ChatResponseDto;
 import com.back.moment.chat.dto.ChatRoomResponseDto;
+import com.back.moment.chat.entity.Chat;
 import com.back.moment.chat.entity.ChatRoom;
 import com.back.moment.chat.repository.ChatRepository;
 import com.back.moment.chat.repository.ChatRoomRepository;
+import com.back.moment.exception.ApiException;
+import com.back.moment.exception.ExceptionEnum;
+import com.back.moment.users.entity.Users;
 import com.back.moment.users.repository.UsersRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Pageable;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -24,33 +33,79 @@ public class ChatService {
     private final ChatRoomRepository chatRoomRepository;
     private final ChatRepository chatRepository;
     private final UsersRepository userRepository;
-
+    private final MongoTemplate mongoTemplate;
     /*
-    이미 사용자 둘간의 채팅방이 존재한다면,
-    기존에 있던 채팅방의 id를 넘겨준다.
-    그렇지 않다면 , 방을 새로 생성해서 반환
+    방에 입장하는 메서드. 우선 채팅을 했던 내역이 있으면,
+    내역들을 보내주어야 한다. 그리고 읽지않음을 모두 읽음으로 변경한다.
+    채팅은 생성된 순으로 먼저 보낸다. 방이 없다면 , chatRoomId , chatList를
+    모두 우선적으로 null로 반환한다. (좋은 방법인지는 모르겠음)
      */
-    public ChatRoomResponseDto createChatRoom(Long sender , Long receriver){
-        userRepository.findById(receriver).orElseThrow(() -> new IllegalStateException("존재하지 않는 사용자"));
-        Optional<ChatRoom> chatRoomByUsers = chatRoomRepository.findChatRoomByUsers(sender, receriver);
+    public ResponseEntity<ChatRoomResponseDto> enterChatRoom(Users userOne , Long userTwoId){
+        Long userOneId = userOne.getId();
+        Users userTwo = userRepository.findById(userTwoId).orElseThrow(() -> new ApiException(ExceptionEnum.NOT_FOUND_USER));
+        Optional<ChatRoom> chatRoomByUsers = chatRoomRepository.findChatRoomByUsers(userOne, userTwo);
         if(chatRoomByUsers.isPresent()){
-            return new ChatRoomResponseDto(chatRoomByUsers.get().getId());
+            boolean isUserOne=false;
+            if(chatRoomByUsers.get().getUserOne().getId().equals(userOneId)) isUserOne = true;
+            Query query = new Query();
+            query.addCriteria(new Criteria().andOperator(
+                    Criteria.where("chatRoomId").is(chatRoomByUsers.get().getId()),
+                    Criteria.where("senderId").is(userTwo.getId()),
+                    Criteria.where("readStatus").is(false)
+                    ));
+            Update update = Update.update("readStatus", true);
+            mongoTemplate.updateMulti(query,update,Chat.class);
+            List<ChatResponseDto> chatResponseDtoList;
+            if(isUserOne){
+                chatResponseDtoList = chatRepository.findByChatRoomIdAndUserOneCanSeeTrueOrderByCreatedAt(chatRoomByUsers.get().getId()).stream().map(c -> ChatResponseDto.from(c)).collect(Collectors.toList());
+            }
+            chatResponseDtoList = chatRepository.findByChatRoomIdAndUserTwoCanSeeTrueOrderByCreatedAt(chatRoomByUsers.get().getId()).stream().map(c -> ChatResponseDto.from(c)).collect(Collectors.toList());
+            return ResponseEntity.ok(new ChatRoomResponseDto(chatRoomByUsers.get().getId(),chatResponseDtoList,userTwo.getProfileImg(),userTwoId,userTwo.getNickName()));
         }
-        ChatRoom chatRoom = chatRoomRepository.save(ChatRoom.of(sender, receriver));
-        return new ChatRoomResponseDto(chatRoom.getId());
+        return ResponseEntity.ok(new ChatRoomResponseDto(null,null,userTwo.getProfileImg(),userTwoId,userTwo.getNickName()));
     }
     /*
-    방에 입장한다. 해당 방 Id에 대한
-    채팅이력이 있다면 , 페이징처리로 반환해준다.
+    방을 생성하는 로직이다.
+    생성하고  chatRoom의 Id를 반환한다.
      */
-    public List<ChatResponseDto> enterChatRoom(Long roomId, Pageable pageable){
-        return chatRepository.findByRoomIdOrderByCreatedAt(roomId,pageable).getContent().stream().map(c-> ChatResponseDto.from(c)).collect(Collectors.toList());
+    public Long createChatRoom(ChatRequestDto chatRequestDto){
+        Users userOne = userRepository.findById(chatRequestDto.getSenderId()).orElseThrow(()-> new ApiException(ExceptionEnum.NOT_FOUND_USER));
+        Users userTwo = userRepository.findById(chatRequestDto.getReceiverId()).orElseThrow(() -> new ApiException(ExceptionEnum.NOT_FOUND_USER));
+        ChatRoom chatRoom = ChatRoom.of(userOne, userTwo);
+        chatRoomRepository.save(chatRoom);
+        return chatRoom.getId();
     }
     /*
     채팅 내용을 저장한다.
     채팅은 MongoDB에 저장한다.
      */
-    public void saveChat(ChatRequestDto chatDto){
-        chatRepository.save(ChatRequestDto.toEntity(chatDto, LocalDateTime.now()));
+    public void saveChat(ChatRequestDto chatRequestDto,Long chatRoomId){
+        ChatRoom chatRoom = chatRoomRepository.findById(chatRoomId).orElseThrow(() -> new ApiException(ExceptionEnum.RUNTIME_EXCEPTION));
+        chatRepository.save(ChatRequestDto.toEntity(chatRequestDto, LocalDateTime.now()));
+        chatRoom.updateLastMessageAt(LocalDateTime.now());
+    }
+    /*
+    모든 채팅방을 모두 가져온다.
+    이 로직에서 MessageAt이 가장 최근인
+    채팅방부터 가져오고 , 현재 로그인된 사용자가 userOne에 존재하는지,
+    userTwo에 존재하는지도 확인해서 , 상대 사용자의 정보도 같이 보내줌.
+     */
+    public ResponseEntity<List<ChatRoomResponseDto>> findAllChatRoom(Users user){
+        ArrayList<ChatRoomResponseDto> chatRoomResponseDtoList = new ArrayList<>();
+        List<ChatRoom> chatRoomList = chatRoomRepository.findAllByUserOneOrUserTwoOrderByLastMessageAtDesc(user, user);
+        for (ChatRoom chatRoom : chatRoomList) {
+            if(chatRoom.getUserOne().getId().equals(user.getId())){
+                Users userTwo = chatRoom.getUserTwo();
+                if(chatRoom.getCanUserOneSee()){
+                    chatRoomResponseDtoList.add(new ChatRoomResponseDto(chatRoom.getId(),null,userTwo.getProfileImg(),userTwo.getId(),userTwo.getNickName()));
+                }
+            }else{
+                Users userOne = chatRoom.getUserOne();
+                if(chatRoom.getCanUserOneSee()){
+                    chatRoomResponseDtoList.add(new ChatRoomResponseDto(chatRoom.getId(),null,userOne.getProfileImg(),userOne.getId(),userOne.getNickName()));
+                }
+            }
+        }
+        return ResponseEntity.ok(chatRoomResponseDtoList);
     }
 }
