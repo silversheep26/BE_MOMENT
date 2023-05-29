e014a0b3b66ccac6bad63a967e189cee61fd2cd2
seokscirbe <seokscribe1970@gmail.com>
2023-05-29 14:53:17 +0900

    Feat: ChatRepository 메서드 추가

--- src/main/java/com/back/moment/chat/repository/ChatRepository.java
+++ src/main/java/com/back/moment/chat/repository/ChatRepository.java
@@ -5,7 +5,11 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.List;
 
-public interface ChatRepository extends MongoRepository<Chat,Long> {
+public interface ChatRepository extends MongoRepository<Chat,String> {
     List<Chat> findByChatRoomIdAndUserOneCanSeeTrueOrderByCreatedAt(Long chatRoomId);
     List<Chat> findByChatRoomIdAndUserTwoCanSeeTrueOrderByCreatedAt(Long chatRoomId);
+    boolean existsChatRoomIdAndUserOneCanSeeTrueAndReadStatusFalseAndReceiverId(Long chatRoomId,Long receiverId);
+    boolean existsChatRoomIdAndUserTwoSeeTrueAndReadStatusFalseAndReceiverId(Long chatRoomId,Long receiverId);
+    List<Chat> findByChatRoomId(Long chatRoomId);
+
 }
