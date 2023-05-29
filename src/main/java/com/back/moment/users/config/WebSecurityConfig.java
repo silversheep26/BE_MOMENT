756d2caaa5a0972af9364ab32de3cdb63795c901
silversheep26 <awindya@naver.com>
2023-05-29 20:50:09 +0900

    feat(spring) : cors 변경

--- src/main/java/com/back/moment/users/config/WebSecurityConfig.java
+++ src/main/java/com/back/moment/users/config/WebSecurityConfig.java
@@ -17,7 +17,10 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.CorsUtils;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @RequiredArgsConstructor
@@ -57,7 +60,7 @@ public class WebSecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.cors().disable()
+        http.cors().and()
                 .csrf().disable()
                 .httpBasic().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
@@ -67,46 +70,66 @@ public class WebSecurityConfig {
                 .requestMatchers("/emails/**").permitAll()
                 .requestMatchers("/main").permitAll()
                 .requestMatchers(PERMIT_URL_ARRAY).permitAll()
-                .anyRequest().authenticated()
+                .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
+
+            .anyRequest().authenticated()
                 // JWT 인증/인가를 사용하기 위한 설정
                 .and().addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
+//    @Bean
+//    public CorsConfigurationSource corsConfigurationSource(){
+//
+//        CorsConfiguration config = new CorsConfiguration();
+//        config.applyPermitDefaultValues();
+//
+//        // 사전에 약속된 출처를 명시
+//        config.addAllowedOrigin("http://localhost:3000");
+//
+//
+//        // 특정 헤더를 클라이언트 측에서 사용할 수 있게 지정
+//        // 만약 지정하지 않는다면, Authorization 헤더 내의 토큰 값을 사용할 수 없음
+//        config.addExposedHeader(JwtUtil.AUTHORIZATION_HEADER);
+//
+//        // 본 요청에 허용할 HTTP method(예비 요청에 대한 응답 헤더에 추가됨)
+//        config.addAllowedMethod("*");
+//
+//        // 본 요청에 허용할 HTTP header(예비 요청에 대한 응답 헤더에 추가됨)
+//        config.addAllowedHeader("*");
+//
+//        // 기본적으로 브라우저에서 인증 관련 정보들을 요청 헤더에 담지 않음
+//        // 이 설정을 통해서 브라우저에서 인증 관련 정보들을 요청 헤더에 담을 수 있도록 해줍니다.
+//        config.setAllowCredentials(true);
+//
+//        // allowCredentials 를 true로 하였을 때,
+//        // allowedOrigin의 값이 * (즉, 모두 허용)이 설정될 수 없도록 검증합니다.
+//        config.validateAllowCredentials();
+//
+//        // 어떤 경로에 이 설정을 적용할 지 명시합니다. (여기서는 전체 경로)
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", config);
+//
+//        return source;
+//    }
+
     @Bean
-    public CorsConfigurationSource corsConfigurationSource(){
-
-        CorsConfiguration config = new CorsConfiguration().applyPermitDefaultValues();
-        config.applyPermitDefaultValues();
-
-        // 사전에 약속된 출처를 명시
-        config.addAllowedOriginPattern("*");
-
-
-
-        // 특정 헤더를 클라이언트 측에서 사용할 수 있게 지정
-        // 만약 지정하지 않는다면, Authorization 헤더 내의 토큰 값을 사용할 수 없음
-        config.addExposedHeader(JwtUtil.AUTHORIZATION_HEADER);
-
-        // 본 요청에 허용할 HTTP method(예비 요청에 대한 응답 헤더에 추가됨)
-        config.addAllowedMethod("*");
-
-        // 본 요청에 허용할 HTTP header(예비 요청에 대한 응답 헤더에 추가됨)
-        config.addAllowedHeader("*");
-
-        // 기본적으로 브라우저에서 인증 관련 정보들을 요청 헤더에 담지 않음
-        // 이 설정을 통해서 브라우저에서 인증 관련 정보들을 요청 헤더에 담을 수 있도록 해줍니다.
-        config.setAllowCredentials(true);
-
-        // allowCredentials 를 true로 하였을 때,
-        // allowedOrigin의 값이 * (즉, 모두 허용)이 설정될 수 없도록 검증합니다.
-        config.validateAllowCredentials();
-
-        // 어떤 경로에 이 설정을 적용할 지 명시합니다. (여기서는 전체 경로)
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", config);
-
-        return source;
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry){
+                registry.addMapping("/**")
+                    .allowedOrigins("*")
+                    .allowedOrigins("http://localhost:8080", "http://localhost:3000", "http://13.124.152.254",
+                        "http://moment-photo.ap-northeast-2.amazonaws.com",  // s3
+                        "http://moment.cadbf9mahvf5.ap-northeast-2.rds.amazonaws.com")    // db(rds)
+//                        .allowedOriginPatterns("*")
+                    .exposedHeaders("ACCESS_KEY", "REFRESH_KEY", "Authorization", "USER_ROLE", "USER_EMAIL")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "PATCH", "OPTIONS")
+                    //.allowedHeaders()
+                    .allowCredentials(true)
+                    .maxAge(3600);
+            }
+        };
     }
-
 }
--- src/main/java/com/back/moment/users/jwt/JwtAuthFilter.java
+++ src/main/java/com/back/moment/users/jwt/JwtAuthFilter.java
@@ -59,7 +59,7 @@ public class JwtAuthFilter extends OncePerRequestFilter {
             } else if (refresh_token == null) {
                 jwtExceptionHandler(response, "AccessToken has Expired. Please send your RefreshToken together.", HttpStatus.BAD_REQUEST.value());
             }
-            // (토큰 만료 && 리프레시 토큰 만료) || 리프레시 토큰이 DB와 비교했을 때 같지 않다면
+            // (토큰 만료 && 리프레시 토큰 만료) || 리프레시 토큰이 DB와 비호교했을 때 같지 않다면
             else {
                 jwtExceptionHandler(response, "RefreshToken Expired", HttpStatus.BAD_REQUEST.value());
                 return;
