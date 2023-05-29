9b39d14c2fd3829b138838fab0dd4cfa47654c62
seokscirbe <seokscribe1970@gmail.com>
2023-05-29 14:52:37 +0900

    Refactor: ChatController 수정
    Body: ChatRoom 삭제와 알림기능 추가

--- src/main/java/com/back/moment/chat/controller/ChatController.java
+++ src/main/java/com/back/moment/chat/controller/ChatController.java
@@ -1,13 +1,13 @@
 package com.back.moment.chat.controller;
 
 import com.back.moment.chat.dto.ChatRequestDto;
+import com.back.moment.chat.dto.ChatResponseDto;
 import com.back.moment.chat.dto.ChatRoomResponseDto;
 import com.back.moment.chat.service.ChatService;
 import com.back.moment.users.security.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -17,7 +17,7 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 public class ChatController {
-    private final ChatService chatRoomService;
+    private final ChatService chatService;
     private final SimpMessagingTemplate msgOperation;
 
     /*
@@ -26,7 +26,7 @@ public class ChatController {
      */
     @GetMapping("chatRoom/enter/{userId}")
     public ResponseEntity<ChatRoomResponseDto> enterChatRoom(@AuthenticationPrincipal UserDetailsImpl userDetails, @PathVariable Long userId){
-        return chatRoomService.enterChatRoom(userDetails.getUsers(),userId);
+        return chatService.enterChatRoom(userDetails.getUsers(),userId);
     }
     /*
     내가 소속되어 있고 , 내가 채팅방을 삭제하지 않은 채팅방 목록들을 가져온다.
@@ -34,23 +34,35 @@ public class ChatController {
      */
     @GetMapping("chatRoom/list")
     public ResponseEntity<List<ChatRoomResponseDto>> findAllChatRoomByUser(@AuthenticationPrincipal UserDetailsImpl userDetails){
-        return chatRoomService.findAllChatRoom(userDetails.getUsers());
+        return chatService.findAllChatRoom(userDetails.getUsers());
+    }
+    /*
+    채팅방 삭제하는 컨트롤러, 사실 삭제를 하지않는다.
+    서비스 단에서 삭제를 요청한 유저에게만 채팅방이
+    보이지 않게끔 하는 컨트롤러
+     */
+    @DeleteMapping("chatRoom/{chatRoomId}")
+    public ResponseEntity<String> deleteChatRoom(@AuthenticationPrincipal UserDetailsImpl userDetails,@PathVariable Long chatRoomId){
+        return chatService.deleteChatRoom(userDetails.getUsers(),chatRoomId);
     }
     /*
     웹소켓을 통해 채팅을 시작하고 , 첫 채팅을 보내면
     채팅방을 생성한다. 우선 채팅방이 없는 상태이면
     /sub/chat/room으로 먼저 채팅방 Id 를 보낸다.
+    그다음 /sub/chat/room/{chatRoomId} 에 chatResponseDto를 보내고,
+    그다음 /sub/chat{receiverId}에 알림을 보냄 , 이쪽으로 값을 따로 보내는건 없다.
      */
     @MessageMapping("/chat/send")
-    @SendTo("/sub/chat/room")
     public void enterChatRoom(ChatRequestDto chatRequestDto){
         Long chatRoomId=null;
         if(chatRequestDto.getChatRoomId()==null){
-            chatRoomId = chatRoomService.createChatRoom(chatRequestDto);
+            chatRoomId = chatService.createChatRoom(chatRequestDto);
             msgOperation.convertAndSend("/sub/chat/room",chatRoomId);
         }
         chatRequestDto.setChatRoomId(chatRoomId);
-        chatRoomService.saveChat(chatRequestDto,chatRoomId);
-        msgOperation.convertAndSend("/sub/chat/room/"+chatRequestDto.getChatRoomId(),chatRequestDto);
+        ChatResponseDto chatResponseDto = chatService.saveChat(chatRequestDto, chatRoomId);
+        msgOperation.convertAndSend("/sub/chat/room/"+chatRequestDto.getChatRoomId(),chatResponseDto);
+        msgOperation.convertAndSend("/sub/chat/"+chatRequestDto.getReceiverId());
     }
+
 }
