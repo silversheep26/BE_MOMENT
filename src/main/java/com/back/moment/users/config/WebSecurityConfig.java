be4b9748b9d82ed07087ae51d17d002ea57c0896
seokscirbe <seokscribe1970@gmail.com>
2023-05-29 21:03:04 +0900

    Fix: WebSecurityConfig 수정

--- src/main/java/com/back/moment/users/config/WebSecurityConfig.java
+++ src/main/java/com/back/moment/users/config/WebSecurityConfig.java
@@ -113,21 +113,6 @@ public class WebSecurityConfig {
 //        return source;
 //    }
     @Bean
-    public CorsConfigurationSource corsConfigurationSource(){
-
-        CorsConfiguration config = new CorsConfiguration();
-        config.applyPermitDefaultValues();
-
-        // 사전에 약속된 출처를 명시
-        config.addAllowedOrigin("http://localhost:3000");
-
-
-
-        // 특정 헤더를 클라이언트 측에서 사용할 수 있게 지정
-        // 만약 지정하지 않는다면, Authorization 헤더 내의 토큰 값을 사용할 수 없음
-        config.addExposedHeader(JwtUtil.AUTHORIZATION_HEADER);
-
-    @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
