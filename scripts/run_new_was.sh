cec931652394596392836430234d5537b607d45e
seokscirbe <seokscribe1970@gmail.com>
2023-05-30 21:00:04 +0900

    Fix: 리다이렉트 설정 없앰

--- scripts/run_new_was.sh
+++ scripts/run_new_was.sh
@@ -30,6 +30,5 @@ fi
 
 # 타켓 포트에 jar파일을 이용해 새로운 서버 실행
 nohup java -jar -Dserver.port=${TARGET_PORT} ${JAR_FILE} > /home/ubuntu/nohup.out 2>&1 &
-sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j REDIRECT --to-port ${TARGET_PORT}
 echo "> Now new WAS runs at ${TARGET_PORT}."
 exit 0
