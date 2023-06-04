88e1963600cf08de57b552ee18b481bd2fc79a56
seokscirbe <seokscribe1970@gmail.com>
2023-06-05 07:21:08 +0900

    Feat: RedisService에 Chat 추가
    Body: 채팅기능에서 Redis를 활용하기 위한 코드 추가

--- src/main/java/com/back/moment/global/service/RedisService.java
+++ src/main/java/com/back/moment/global/service/RedisService.java
@@ -1,17 +1,25 @@
 package com.back.moment.global.service;
 
+import com.back.moment.chat.entity.Chat;
+import com.back.moment.chat.repository.ChatRepository;
+import com.back.moment.exception.ApiException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Service;
 
 import java.time.Duration;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
 public class RedisService {
 
     private final RedisTemplate<String, String> redisTemplate;
+    private final ChatRepository chatRepository;
+    private final ObjectMapper objectMapper;
 
     public void setRefreshValues(String userId, String key) {
         redisTemplate.opsForHash().put("Refresh",userId,key);
@@ -23,6 +31,17 @@ public class RedisService {
         redisTemplate.expire("Code",Duration.ofMinutes(10L));
     }
 
+    public void setChatValues(Chat chat,Long chatRoomId,String chatId){
+        String chatAsJson = null;
+        try {
+            chatAsJson = objectMapper.writeValueAsString(chat);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+        redisTemplate.opsForHash().put("Chat"+chatRoomId,chatId,chatAsJson);
+        redisTemplate.expire("Chat"+chatRoomId,Duration.ofMinutes(24 * 60L));
+    }
+
     public String getRefreshToken(String userId){
         return (String) redisTemplate.opsForHash().get("Refresh",userId);
     }
@@ -31,10 +50,33 @@ public class RedisService {
         return (String) redisTemplate.opsForHash().get("Code",userId);
     }
 
+    public Chat getChat(Long chatRoomId,String chatId){
+        String chatJson = (String) redisTemplate.opsForHash().get("Chat" + chatRoomId, chatId);
+        try {
+            return objectMapper.readValue(chatJson, Chat.class);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    public void saveChatsToDB(String hashKey){
+        Map<Object, Object> entries = redisTemplate.opsForHash().entries(hashKey);
+        for (Object key : entries.keySet()) {
+            String json =(String) entries.get(key);
+            Chat chat = null;
+            try {
+                chat = objectMapper.readValue(json, Chat.class);
+            } catch (JsonProcessingException e) {
+                throw new RuntimeException(e);
+            }
+            chatRepository.save(chat);
+        }
+        redisTemplate.delete(hashKey);
+    }
+
 
     public void deleteValues(String key){
-        System.out.println(key);
-
         redisTemplate.delete(key);
     }
 }
