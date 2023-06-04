180a991f88f87d7edfa556ee61f843ff52eb5df0
seokscirbe <seokscribe1970@gmail.com>
2023-06-05 07:22:44 +0900

    Feat: Redis에서 사용하기 위한 키값으로 uuid 추가
    Body: Redis에서 키값으로 사용하기 위한 필드가 필요해서 uuid를 추가

--- src/main/java/com/back/moment/chat/entity/Chat.java
+++ src/main/java/com/back/moment/chat/entity/Chat.java
@@ -1,16 +1,19 @@
 package com.back.moment.chat.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Data
 @Document(collection = "chat")
 public class Chat {
     @Id
     private String id;
+    private String uuid;
     private String message;
     private Long senderId; // 채팅을 보내는 유저의 아이디
     private Long receiverId; // 채팅을 받는 유저의 아이디
@@ -23,10 +26,15 @@ public class Chat {
         this.senderId = senderId;
         this.chatRoomId = chatRoomId;
         this.createdAt = createdAt;
+        this.uuid = UUID.randomUUID().toString().substring(0,8);
     }
 
     public static Chat of(String message, Long senderId, Long chatRoomId, LocalDateTime createdAt){
         return new Chat(message,senderId,chatRoomId,createdAt);
+    }
+
+    public void updateReadStatus(){
+        this.readStatus = true;
     }
 }
 
