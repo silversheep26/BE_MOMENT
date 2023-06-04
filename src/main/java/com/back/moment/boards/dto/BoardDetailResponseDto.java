aaa75dc783510aa9fec5d9912402ce8e627c5be8
silversheep26 <awindya@naver.com>
2023-06-05 01:28:10 +0900

    fix(spring) : board 상세 조회 수정 feedimg + profile 이미지 수정

--- src/main/java/com/back/moment/boards/dto/BoardDetailResponseDto.java
+++ src/main/java/com/back/moment/boards/dto/BoardDetailResponseDto.java
@@ -1,31 +1,39 @@
 package com.back.moment.boards.dto;
 
 import com.back.moment.boards.entity.Board;
+import com.back.moment.photos.dto.OnlyPhotoResponseDto;
+import com.back.moment.photos.entity.Photo;
 import com.back.moment.users.entity.RoleEnum;
+import java.util.Comparator;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.management.relation.Role;
 import java.util.List;
 
 @Getter
 @NoArgsConstructor
 public class BoardDetailResponseDto {
     private Long hostId;
-    private String profileUrl;
     private String nickName;
     private String title;
     private String contents;
     private RoleEnum role;
     private List<String> tag_boardList;
+    private String profileUrl;
+    private List<OnlyPhotoResponseDto> feedImgUrl;
 
     public BoardDetailResponseDto(Board board){
         this.hostId = board.getUsers().getId();
-        this.profileUrl = board.getBoardImgUrl();
         this.nickName = board.getUsers().getNickName();
         this.title = board.getTitle();
         this.contents = board.getContents();
         this.role = board.getUsers().getRole();
         this.tag_boardList = board.getTagListWithWell();
+        this.profileUrl = board.getUsers().getProfileImg();
+        this.feedImgUrl = board.getUsers().getPhotoList().stream()
+            .map(OnlyPhotoResponseDto::new)
+            .sorted(Comparator.comparingInt(OnlyPhotoResponseDto::getLoveCnt).reversed())
+            .limit(6)
+            .toList();
     }
 }
