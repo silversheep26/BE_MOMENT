97401025b80bf9a684d3e0eab914f3706d3ee18b
DongKyu Lee <dldlehdrbrb@gmail.com>
2023-06-05 11:46:48 +0900

    Fix : 피드 생성 시 장소 급여조건 지원방법 모집마감일 null 값이여도 입력 가능하도록 수정 + 게시물 상세 조회 시 쿼리 요청 리펙토링

--- src/main/java/com/back/moment/boards/service/BoardService.java
+++ src/main/java/com/back/moment/boards/service/BoardService.java
@@ -45,6 +45,7 @@ public class BoardService {
     public ResponseEntity<Void> createBoard(BoardRequestDto boardRequestDto, Users users, MultipartFile boardImg){
         Board board = new Board();
         if(users.getRole() != RoleEnum.NONE) {
+
             board.saveBoard(boardRequestDto, users);
             if (!boardImg.isEmpty()) {
                 try {
@@ -145,8 +146,6 @@ public class BoardService {
     @Transactional(readOnly = true)
     public ResponseEntity<BoardDetailResponseDto> getBoard(Long boardId, Users users){
         Board board = existBoard(boardId);
-//        BoardDetailResponseDto detailResponseDto = new BoardDetailResponseDto(board, users);
-
 
         return new ResponseEntity<>(new BoardDetailResponseDto(board), HttpStatus.OK);
     }
@@ -174,7 +173,7 @@ public class BoardService {
     }
 
     public Board existBoard(Long boardId){
-        return boardRepository.findById(boardId).orElseThrow(
+        return boardRepository.findExistBoard(boardId).orElseThrow(
                 () -> new ApiException(ExceptionEnum.NOT_FOUND_POST)
         );
     }
