90e7f27450cab6e63a3236ce5b9b4ac58e0d08b7
seokscirbe <seokscribe1970@gmail.com>
2023-06-07 18:30:06 +0900

    Chore: TestController 4번째

--- src/main/java/com/back/moment/test/TestController.java
+++ src/main/java/com/back/moment/test/TestController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/test")
     public String test() {
-        return "test33333";
+        return "test4444444";
     }
 }
