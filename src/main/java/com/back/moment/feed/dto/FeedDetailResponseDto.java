71400dc61a44fbc1a60dd4f480f28590cf74a942
DongKyu Lee <dldlehdrbrb@gmail.com>
2023-06-10 00:00:47 +0900

    Fix : 피드 등록시간 수정

--- src/main/java/com/back/moment/feed/dto/FeedDetailResponseDto.java
+++ src/main/java/com/back/moment/feed/dto/FeedDetailResponseDto.java
@@ -34,6 +34,6 @@ public class FeedDetailResponseDto {
         this.role = photo.getUsers().getRole();
         this.contents = photo.getContents();
         this.tag_photoList = photo.getTagListWithWell();
-        this.createdTime = (photo.getCreatedAt() != null) ? photo.getCreatedAt().plusHours(9L) : LocalDateTime.now().plusHours(9L);
+        this.createdTime = photo.getCreatedAt().plusHours(9L);
     }
 }
