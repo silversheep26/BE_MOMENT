7ae8c2cfb7beb980f271bf252dd1931cc9eeb942
DongKyu Lee <dldlehdrbrb@gmail.com>
2023-06-09 18:19:33 +0900

    Fix : 사진 버켓 변경

--- src/main/java/com/back/moment/users/jwt/JwtAuthFilter.java
+++ src/main/java/com/back/moment/users/jwt/JwtAuthFilter.java
@@ -56,6 +56,7 @@ public class JwtAuthFilter extends OncePerRequestFilter {
                 jwtUtil.setHeaderAccessToken(response, newAccessToken);
                 // Security context에 인증 정보 넣기
                 setAuthentication(email);
+                return;
             } else if (refresh_token == null) {
                 jwtExceptionHandler(response, "AccessToken has Expired. Please send your RefreshToken together.", HttpStatus.BAD_REQUEST.value());
             }
