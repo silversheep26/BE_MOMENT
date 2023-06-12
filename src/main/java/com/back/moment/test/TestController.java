5a4d9aafb045d13a6688f64a3fbf32df16b9e5db
seokscirbe <seokscribe1970@gmail.com>
2023-06-12 10:24:31 +0900

    Fix: TestController 작성

--- src/main/java/com/back/moment/test/TestController.java
+++ src/main/java/com/back/moment/test/TestController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/test")
     public String test() {
-        return "test5555555";
+        return "test8081";
     }
 }
