d690fbba1822def9333d0011d5b58230a5e4c41b
silversheep26 <awindya@naver.com>
2023-06-16 03:04:43 +0900

    fix(spring) : 예외처리 추가

--- src/main/java/com/back/moment/exception/GlobalExceptionHandler.java
+++ src/main/java/com/back/moment/exception/GlobalExceptionHandler.java
@@ -1,10 +1,12 @@
 package com.back.moment.exception;
 
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.client.HttpClientErrorException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -32,9 +34,28 @@ public class GlobalExceptionHandler {
         return ErrorResponse.toResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR, e.toString());
     }
 
-    @ExceptionHandler(value = IllegalStateException.class)
-    public ResponseEntity<ErrorResponse> handleIllegalStateException(IllegalStateException e) {
+    @ExceptionHandler(value = IllegalArgumentException.class)
+    public ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException e) {
+        e.printStackTrace();
+        return ErrorResponse.toResponseEntity(HttpStatus.BAD_REQUEST, e.toString());
+    }
+
+    @ExceptionHandler(value = HttpClientErrorException.class)
+    public ResponseEntity<ErrorResponse> handleHttpClientErrorException(HttpClientErrorException e) {
+        e.printStackTrace();
+        return ErrorResponse.toResponseEntity(HttpStatus.BAD_REQUEST, e.toString());
+    }
+
+    @ExceptionHandler(DataAccessException.class)
+    public ResponseEntity<ErrorResponse> handleDataAccessException(DataAccessException e) {
         e.printStackTrace();
         return ErrorResponse.toResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR, e.toString());
     }
+
+    @ExceptionHandler(NullPointerException.class)
+    public ResponseEntity<ErrorResponse> handleNullPointerException(NullPointerException e) {
+        e.printStackTrace();
+        return ErrorResponse.toResponseEntity(HttpStatus.BAD_REQUEST, e.toString());
+    }
+
 }
