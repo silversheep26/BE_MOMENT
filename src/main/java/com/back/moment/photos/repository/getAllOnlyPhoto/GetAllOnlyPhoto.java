7f009aa1256bc0f3b726028d8783d035f366d4f1
DongKyu Lee <dldlehdrbrb@gmail.com>
2023-06-17 01:29:43 +0900

    Fix : 마이페이지 조회 시 피드 list 수정

---
+++ src/main/java/com/back/moment/photos/repository/getAllOnlyPhoto/GetAllOnlyPhoto.java
@@ -0,0 +1,9 @@
+package com.back.moment.photos.repository.getAllOnlyPhoto;
+
+import com.back.moment.photos.dto.OnlyPhotoResponseDto;
+
+import java.util.List;
+
+public interface GetAllOnlyPhoto {
+    List<OnlyPhotoResponseDto> findAllOnlyPhoto();
+}
