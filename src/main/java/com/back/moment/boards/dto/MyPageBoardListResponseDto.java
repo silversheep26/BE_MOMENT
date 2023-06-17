f9199f3c8afbf0a7182b28bce3f6364a100f8c1b
DongKyu Lee <dldlehdrbrb@gmail.com>
2023-06-17 12:27:24 +0900

    Fix : createdAt 응답시 9시간 더하는 로직 제거

--- src/main/java/com/back/moment/boards/dto/MyPageBoardListResponseDto.java
+++ src/main/java/com/back/moment/boards/dto/MyPageBoardListResponseDto.java
@@ -32,7 +32,7 @@ public class MyPageBoardListResponseDto {
         this.profileImgUrl = board.getUsers().getProfileImg();
         this.location = board.getLocation();
         this.boardImgUrl = board.getBoardImgUrl();
-        this.createdTime = board.getCreatedAt().plusHours(9L);
+        this.createdTime = board.getCreatedAt();
         this.tag_boardList = board.getTagListWithWell();
     }
 }
