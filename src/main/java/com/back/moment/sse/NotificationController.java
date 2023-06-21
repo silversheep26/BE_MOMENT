0e34ba520ec432a08602ee3acafbac14bb8590c9
seokscirbe <seokscribe1970@gmail.com>
2023-06-21 22:49:46 +0900

    Fix: CORS 설정

--- src/main/java/com/back/moment/sse/NotificationController.java
+++ src/main/java/com/back/moment/sse/NotificationController.java
@@ -14,6 +14,9 @@ public class NotificationController {
     @GetMapping(value = "/chat/alarm/{userId}",produces = "text/event-stream")
     public SseEmitter chatAlarm(@PathVariable Long userId, HttpServletResponse response){
         response.setHeader("Connection", "keep-alive");
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Expose-Headers", "*");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
         return notificationService.subscribe(userId);
     }
 }
