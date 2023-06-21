54299653d140f7477b680b88749e2fa2156a1791
seokscirbe <seokscribe1970@gmail.com>
2023-06-22 00:03:55 +0900

    Fix: Emitter Timeout설정

--- src/main/java/com/back/moment/sse/NotificationController.java
+++ src/main/java/com/back/moment/sse/NotificationController.java
@@ -10,13 +10,8 @@ import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 @RequiredArgsConstructor
 public class NotificationController {
     private final NotificationService notificationService;
-    @CrossOrigin(origins = "https://www.momentapp.site",methods = {RequestMethod.GET,RequestMethod.OPTIONS})
     @GetMapping(value = "/chat/alarm/{userId}",produces = "text/event-stream")
     public SseEmitter chatAlarm(@PathVariable Long userId, HttpServletResponse response){
-        response.setHeader("Connection", "keep-alive");
-        response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Expose-Headers", "*");
-        response.setHeader("Access-Control-Allow-Credentials", "true");
         return notificationService.subscribe(userId);
     }
 }
