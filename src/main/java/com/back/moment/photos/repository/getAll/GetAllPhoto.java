f172d758cc6d032d389a53fb9581bd96a4c788b0
DongKyu Lee <dldlehdrbrb@gmail.com>
2023-06-24 23:54:15 +0900

    Feat : 피드 전체 조회 성능 개선

--- src/main/java/com/back/moment/photos/repository/getAll/GetAllPhoto.java
+++ src/main/java/com/back/moment/photos/repository/getAll/GetAllPhoto.java
@@ -1,9 +1,11 @@
 package com.back.moment.photos.repository.getAll;
 
 import com.back.moment.photos.entity.Photo;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface GetAllPhoto {
-    List<Photo> getAllPhoto();
+    Page<Photo> getAllPhoto(Pageable pageable);
 }
